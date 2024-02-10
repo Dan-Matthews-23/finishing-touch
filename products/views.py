51dd689d2210c64b7948fe1715f4d08a78feda1b
Dan Matthews <dan.matthews1987@gmail.com>
2024-02-10 15:47:16 +0000

    UPDATED views, urls to limit product listing by category ID

--- products/views.py
+++ products/views.py
@@ -3,9 +3,19 @@ from .models import Products
 
 # Create your views here.
 
-def list_all_products(request):
-    products = Products.objects.all()
+# Display all breads
+def list_all_breads(request):
+    #products = Products.objects.all()
+    breads = Products.objects.filter(category_id=1)
     context = {
-        'products' : products,
+        'breads' : breads,
+    }
+    return render(request, 'products/products.html', context)
+
+# Display all fillings
+def list_all_fillings(request):    
+    fillings = Products.objects.filter(category_id=2)
+    context = {
+        'fillings' : fillings,
     }
     return render(request, 'products/products.html', context)
