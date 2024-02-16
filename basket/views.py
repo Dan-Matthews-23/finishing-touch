9370a59feb8b8428e61c0665adb975a8469cadb2
Dan Matthews <dan.matthews1987@gmail.com>
2024-02-16 15:39:49 +0000

    UPDATED views, script.js, sandwiches with additional content

--- basket/views.py
+++ basket/views.py
@@ -2,3 +2,20 @@ from django.shortcuts import render
 
 def basket(request):
     return render(request, 'basket/basket.html')
+
+
+def add_items_to_basket(request, product_id):  
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if product_id in list(bag.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
+    
