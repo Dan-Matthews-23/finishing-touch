5393e72fdeaab06f11b15c1015070775f92197b0
Dan Matthews <dan.matthews1987@gmail.com>
2024-02-22 18:40:11 +0000

    UPDATED script, sandwiches to correct JS logical errors

--- static/js/script.js
+++ static/js/script.js
@@ -115,27 +115,23 @@ function openModal(event) {
 const openModalButtons = document.querySelectorAll('.open-modal');
 openModalButtons.forEach(button => {
     button.addEventListener('click', function (event) {
-        // const productId = event.target.dataset.data - productID; open-modal-value
-        const productId = parseInt(document.getElementById("open-modal-value").value, 10);
-        const productName = event.target.dataset.productName;
-        const productPrice = event.target.dataset.productPrice;
-        // You now have the data to use or pass to your updateQuantity function
-        updateQuantity(true, productId, productName, productPrice); // Example if updateQuantity handles adding 
-
+        const productId = button.value;
+        console.log(productId);
+        updateQuantity(true, productId);
     });
 });
 
 
-function updateQuantity(isIncrease, productId, productName, productPrice) { // Pass product_id if not using event
+function updateQuantity(isIncrease, productId) { // Pass product_id if not using event
     if (isIncrease) {
         const itemIndex = orderArray.findIndex(item => item.product_id === productId);
-        
+
         //console.log(`ID is ${productId} and its type is ${typeof productId}`)
         if (itemIndex !== -1) {
             console.log(`Yes, there is an item here with the product ID of ${productId}. We will update the quantity to ${orderArray[itemIndex].product_quantity}`);
             //let new_quantity = 0;
-            orderArray[itemIndex].product_quantity += 1;
-            
+            //orderArray[itemIndex].product_quantity += 1;
+
             //orderArray[itemIndex].product_quantity = new_quantity; // Update existing property
             //orderArray[itemIndex].product_quantity = productName,
             //orderArray[itemIndex].product_name,
@@ -144,14 +140,15 @@ function updateQuantity(isIncrease, productId, productName, productPrice) { // P
             //product_quantity: new_quantity
 
             //orderArray[itemIndex].update(new_values);
-            localStorage.setItem("orderArray", JSON.stringify(orderArray));
+            //localStorage.setItem("orderArray", JSON.stringify(orderArray));
             //new_values = [];
             //console.log(orderArray);
             //console.log("The quantity was increased. It is now " + orderArray[itemIndex].product_quantity);
             //console.log(`The index is ${itemIndex}`)
             //console.log(`The product ID is ${orderArray[itemIndex].product_id} and its type is ${typeof orderArray[itemIndex].product_id} `)
-            return productId;
+            //return productId;
         } else {
+            /*
             let quantity = 0;
             quantity += 1;
             newItem = {
@@ -161,8 +158,8 @@ function updateQuantity(isIncrease, productId, productName, productPrice) { // P
                 original_price: parseFloat(productPrice),
                 product_quantity: quantity
             };
-            orderArray.push(newItem);
-            //console.log("Item not found, so we've created it instead");
+            orderArray.push(newItem);*/
+            console.log(`Item not found, so we've created it instead. The product ID is ${productId} and its type is ${typeof productId}`);
             //console.log(itemIndex);
             //console.log(orderArray);
             //console.log(`All details have been pulled from the orderArray and are: Product ID: ${productId}, Product Name: ${productName}  `)
