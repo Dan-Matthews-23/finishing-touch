f846bb4611fd799be3ccbd1bdab472ad06e097ee
Dan Matthews <dan.matthews1987@gmail.com>
2024-02-28 13:52:58 +0000

    CREATED signals.py, UPDATED admin.py with customised models

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Orders
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = (
+        'order_id',       
+        'order_number',
+        'customer_name',
+        'customer_email',
+        'customer_tel_number',
+        'customer_address_one',
+        'customer_address_two',
+        'customer_address_three',
+        'customer_address_four',
+        'customer_postcode',
+        'order_date',
+        'delivery_cost',
+        'order_cost',
+        'total_cost',
+    )
+    ordering = ('order_id',)  
 
 # Register your models here.
+admin.site.register(Orders, OrdersAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -16,14 +16,43 @@ class Orders(models.Model):
     customer_address_four = models.CharField(max_length=50)
     customer_postcode = models.CharField(max_length=50)
     order_date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=2.00)
     order_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0)
     total_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0)
 
-class OrderPlaceholder:
+    def _generate_order_number(self):        
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):        
+        self.order_total = self.lineitems.aggregate(Sum('total_cost'))['total_cost__sum']       
+        
+        #if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+        #    self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+       # else:
+        #    self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):      
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderPlaceholder(models.Model):
     order = models.ForeignKey(Orders, null=False, blank=False, on_delete=models.CASCADE)
     item = models.ForeignKey(Products, null=False, blank=False, on_delete=models.CASCADE)
-    quantity = models.IntegerField(max_digits=6, decimal_places=2, null=False, blank=False, default=0)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
     total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0, editable=False)
+
+    def save(self, *args, **kwargs):       
+        self.total_cost = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
 
 
