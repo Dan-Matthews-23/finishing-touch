6291648ab1ffb8485ab79aaca16468febfc17631
Dan Matthews <dan.matthews1987@gmail.com>
2024-02-28 12:25:46 +0000

    UPDATED checkout/models to create order and orderplaceholder models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+import uuid
+from products.models import Products
 
-# Create your models here.
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    order_number = models.IntegerField(null=False, blank=False, editable=False)
+    customer_name = models.CharField(max_length=50, null=False, blank=False)
+    customer_email = models.CharField(max_length=50, null=False, blank=False)
+    customer_tel_number = models.CharField(max_length=50, null=False, blank=False)
+    customer_address_one = models.CharField(max_length=50, null=False, blank=False)
+    customer_address_two = models.CharField(max_length=50, null=False, blank=False)
+    customer_address_three = models.CharField(max_length=50, null=False, blank=False)
+    customer_address_four = models.CharField(max_length=50)
+    customer_postcode = models.CharField(max_length=50)
+    order_date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0)
+    order_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0)
+    total_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0)
+
+class OrderPlaceholder:
+    order = models.ForeignKey(Orders, null=False, blank=False, on_delete=models.CASCADE)
+    item = models.ForeignKey(Products, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(max_digits=6, decimal_places=2, null=False, blank=False, default=0)
+    total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0, editable=False)
+
+
