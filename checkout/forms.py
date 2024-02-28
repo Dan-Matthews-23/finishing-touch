de6d80c889f0e1ebc8920d4b83ce4bb1c23be5b0
Dan Matthews <dan.matthews1987@gmail.com>
2024-02-28 14:57:06 +0000

    CREATED forms.py, installed crispy forms package

---
+++ checkout/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = (         
+        'customer_name',
+        'customer_email',
+        'customer_tel_number',
+        'customer_address_one',
+        'customer_address_two',
+        'customer_address_three',
+        'customer_address_four',
+        'customer_postcode',  )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+        'customer_name', 'Full Name',
+        'customer_email', 'Email Address',
+        'customer_tel_number', 'Phone Number',
+        'customer_address_one', ' House/Flat number',
+        'customer_address_two', 'Street Name',
+        'customer_address_three', 'Town/City',
+        'customer_address_four', 'County',
+        'customer_postcode', 'Postcode',
+        }
+
+        self.fields['customer_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
