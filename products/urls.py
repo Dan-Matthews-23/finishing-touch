2aae58a9a12a169f121471cbdb00ebe9a7683bef
Dan Matthews <dan.matthews1987@gmail.com>
2024-03-06 07:52:37 +0000

    UPDATED checkout/views, /urls, /products/views, /urls, sandwitches.html, settings and stripe_elements to create and handle errors with sessions

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('prepacked_sandwiches/', views.prepacked_sandwiches, name='prepacked_sandwiches'),
-    path('sandwich_creator/', views.list_sandwich_creator_items, name='sandwich_creator'),      
+    path('sandwich_creator/', views.list_sandwich_creator_items, name='sandwich_creator'),
+    path('add_to_basket/', views.add_to_basket, name='add_to_basket'),          
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Products, Category
+import json
 
 # Create your views here.
 
@@ -18,4 +19,35 @@ def list_sandwich_creator_items(request):
     context = {
         'sandwich_items' : sandwich_items,
     }
-    return render(request, 'products/sandwich-creator.html', context)
+    return render(request, 'products/sandwich-creator.html', context) 
+
+
+def add_to_basket(request):
+    if request.method == "POST":
+        orderArrayData = request.POST.get("orderData")
+        order_items = json.loads(orderArrayData)
+        total_price = 0
+
+        for item in order_items:
+            try:
+                default_price = float(item['default_price'])              
+                quantity = item['product_quantity']
+                
+                total_price = float(total_price)
+                total_price += default_price * quantity
+                total_price = f"{total_price:.2f}"              
+                
+
+            except KeyError as e:
+                print(f"Missing key '{e}' in order item: {item}")
+
+        context = {
+            'order_items': order_items,
+            'total_price': total_price
+        }
+        request.session['basket'] = context
+        print(request.session['basket'])
+        return render(request, 'basket/basket.html', context)
+
+    elif request.method == "GET":
+        return render(request, 'basket/basket.html')
