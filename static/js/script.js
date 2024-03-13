dedf94a70978782f2e8904c6138ac11d25cfa5ab
Dan Matthews <dan.matthews1987@gmail.com>
2024-03-13 12:37:54 +0000

    UPDATED script.js, sandwiches.html, base.css  to correct errors in order display

--- static/js/script.js
+++ static/js/script.js
@@ -1,43 +1,31 @@
 //localStorage.removeItem("selectedItem");
 //localStorage.removeItem("orderArray");
 
-/*
-const order_div = document.getElementById("display_order");
-let orderArray = JSON.parse(localStorage.getItem("orderArray")) || [];
-orderArray = orderArray.map(item => ({
-    ...item,
-    default_price: item.default_price,
-    default_price: item.default_price
 
-}));
-updateOrderDisplay();
 
-//console.log(orderArray);
 
-*/
-
+// Variables
 let order_div = document.getElementById("display_order");
 let total_cost = document.getElementById("total_cost");
-
 let orderArray = JSON.parse(localStorage.getItem("orderArray")) || [];
-
-if (orderArray.length > 0) {
-    orderArray.map((entry, index) => `
-    ${entry.product_id}
-    <br>
-    ${entry.product_name}
-    <br>
-    ${entry.price}
-    <br>
-    ${entry.totalPrice}
-    <br>         
-        `).join('');
-}
 const jsonData = JSON.stringify(orderArray);
 document.getElementById('orderData').value = jsonData;
+const selectProductBtns = document.querySelectorAll('.open-modal');
 
-if (orderArray.length > 0) {
-    order_div.innerHTML = orderArray.map((entry, index) => `
+// Event Listeners
+selectProductBtns.forEach(btn => {
+    btn.addEventListener('click', openModal);
+});
+
+
+
+
+
+
+
+function updateOrder() {
+    if (orderArray.length > 0) {
+        order_div.innerHTML = orderArray.map((entry, index) => `
             ${entry.product_name} (x ${entry.product_quantity})<br>
             &pound;${entry.price}<br>
             <br>
@@ -45,41 +33,43 @@ if (orderArray.length > 0) {
             <br><br>
         `).join('');
 
-    let totalSum = orderArray.reduce((accumulator, entry) => {
-        entry.price = entry.price;
+        let totalSum = orderArray.reduce((accumulator, entry) => {
+            entry.price = entry.price;
 
-        if (isNaN(entry.price)) {
-            console.error("Failed to convert price to float:", entry);
-        }
+            if (isNaN(entry.price)) {
+                console.error("Failed to convert price to float:", entry);
+            }
 
-        return accumulator + entry.price;
-    }, 0);
+            return accumulator + entry.price;
+        }, 0);
 
-    total_cost.innerHTML = (`Total: ${totalSum}`);
+        total_cost.innerHTML = (`Total: ${totalSum}`);
 
 
-    const deleteButtons = order_div.querySelectorAll('#delete_item');
-    deleteButtons.forEach(button => {
-        button.addEventListener('click', () => deleteItem(button.dataset.index));
-    });
-} else {
-    order_div.innerHTML = "Your basket is empty";
+        const deleteButtons = order_div.querySelectorAll('#delete_item');
+        deleteButtons.forEach(button => {
+            button.addEventListener('click', () => deleteItem(button.dataset.index));
+        });
+    } else {
+        order_div.innerHTML = "Your basket is empty";
+    }
+
 }
+
+
+
 
 function deleteItem(index) {
     orderArray.splice(index, 1);
     localStorage.setItem("orderArray", JSON.stringify(orderArray));
-    updateOrderDisplay();
+    updateOrder();
 }
 
-const selectProductBtns = document.querySelectorAll('.open-modal');
-selectProductBtns.forEach(btn => {
-    btn.addEventListener('click', openModal);
-});
+
 
 function openModal(event) {
     const modal = document.getElementById("modal-div");
-    document.getElementById('quantity-div').innerHTML = 1;
+    //document.getElementById('quantity-div').innerHTML = 1;
     const confirmBtn = document.querySelector('.select-product-btn');
     modal.classList.add("show");
     const decreaseBtn = document.getElementById('decrease-quantity');
@@ -101,17 +91,27 @@ function openModal(event) {
 const openModalButtons = document.querySelectorAll('.open-modal');
 openModalButtons.forEach(modalBtn => {
     modalBtn.addEventListener('click', function (event) {
+
         const productIdFromBtn = modalBtn.value;
         const productNameFromBtn = this.dataset.productName;
         const productPriceFromBtn = this.dataset.productPrice;
         const foundProduct = orderArray.find(item => item.product_id === productIdFromBtn);
         if (foundProduct) {
+            //document.getElementById('quantity-div').innerHTML = 1;
+            
+
+            
             product_id = productIdFromBtn;
             product_name = foundProduct.product_name;
             default_price = foundProduct.default_price;
 
+            const resetQuantity = orderArray.findIndex(a => a.product_id == product_id);
+            document.getElementById('quantity-div').innerHTML = orderArray[resetQuantity].product_quantity;
+            console.log(orderArray[resetQuantity].product_quantity)
+
             msg = "Product was found";
         } else {
+            document.getElementById('quantity-div').innerHTML = 1;
             product_id = productIdFromBtn;
             product_name = productNameFromBtn;
             default_price = productPriceFromBtn
@@ -130,36 +130,51 @@ openModalButtons.forEach(modalBtn => {
         const increaseBtn = document.getElementById('increase-quantity');
         increaseBtn.addEventListener('click', () => updateQuantity(product_id, product_name, default_price));
     });
+
+
+
+
+    updateOrder();
+
+
+
 });
 
-function updateQuantity(product_id, product_name, default_price) { // Pass product_id if not using event
+function updateQuantity(product_id, product_name, default_price) {
 
     if (product_id) { product_id = product_id; }
     if (default_price) { default_price = default_price; }
+
     const pullOrderArrayIndex = orderArray.findIndex(a => a.product_id == product_id);
     if (pullOrderArrayIndex !== -1) {
         if (!isNaN(orderArray[pullOrderArrayIndex].product_quantity)) {
             orderArray[pullOrderArrayIndex].product_quantity += 1
         } else {
-            orderArray[pullOrderArrayIndex].product_quantity = 0;
+            orderArray[pullOrderArrayIndex].product_quantity = 1;
         }
+
         document.getElementById('quantity-div').innerHTML = orderArray[pullOrderArrayIndex].product_quantity;
         orderArray[pullOrderArrayIndex].price = orderArray[pullOrderArrayIndex].default_price * orderArray[pullOrderArrayIndex].product_quantity;
         orderArray[pullOrderArrayIndex].price = orderArray[pullOrderArrayIndex].price.toFixed(2);
-        
+
         localStorage.setItem("orderArray", JSON.stringify(orderArray));
 
-        console.log(`The value of produdct_id is ${orderArray[pullOrderArrayIndex].product_id} and its type is ${typeof orderArray[pullOrderArrayIndex].product_id}`);
-        console.log(`The value of product_name is ${orderArray[pullOrderArrayIndex].product_name} and its type is ${typeof orderArray[pullOrderArrayIndex].product_name}`);
-        console.log(`The value of price is ${orderArray[pullOrderArrayIndex].price} and its type is ${typeof orderArray[pullOrderArrayIndex].price}`);
+        //console.log(`The value of produdct_id is ${orderArray[pullOrderArrayIndex].product_id} and its type is ${typeof orderArray[pullOrderArrayIndex].product_id}`);
+        //console.log(`The value of product_name is ${orderArray[pullOrderArrayIndex].product_name} and its type is ${typeof orderArray[pullOrderArrayIndex].product_name}`);
+        //console.log(`The value of price is ${orderArray[pullOrderArrayIndex].price} and its type is ${typeof orderArray[pullOrderArrayIndex].price}`);
         //console.log(`The value of price_calc is ${orderArray[pullOrderArrayIndex].price_calc} and its type is ${typeof orderArray[pullOrderArrayIndex].price_calc}`);
-        console.log(`The value of quantity is ${orderArray[pullOrderArrayIndex].product_quantity} and its type is ${typeof orderArray[pullOrderArrayIndex].product_quantity}`);
-        console.log(`The value of default_price is ${orderArray[pullOrderArrayIndex].default_price} and its type is ${typeof orderArray[pullOrderArrayIndex].default_price}`);
+        //console.log(`The value of quantity is ${orderArray[pullOrderArrayIndex].product_quantity} and its type is ${typeof orderArray[pullOrderArrayIndex].product_quantity}`);
+        //console.log(`The value of default_price is ${orderArray[pullOrderArrayIndex].default_price} and its type is ${typeof orderArray[pullOrderArrayIndex].default_price}`);
     } else {
         console.log("Product not found in the order array");
     }
+    updateOrder()
 }
+
+
+
 function confirmAndClose() {
     document.getElementById("modal-div").classList.remove("show");
     document.getElementById("modal-div").classList.add("hide");
+    updateOrder()
 }
