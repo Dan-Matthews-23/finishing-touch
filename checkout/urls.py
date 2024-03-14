4fbd33e3b43dd9bbb2dcc7b024c7e1ba6999b260
Dan Matthews <dan.matthews1987@gmail.com>
2024-03-14 16:26:04 +0000

    UPDATED checkout/urls, settings.py with updated code for webhooks

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .webhooks import webhook
 
 urlpatterns = [
     #path('', views.checkout, name='checkout'),
