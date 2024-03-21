2aaf9336fca84ada259407bd9733525d7f39a323
Dan Matthews <dan.matthews1987@gmail.com>
2024-03-21 12:31:23 +0000

    UPDATED accounts/views, /models, profile,html  to test a new profile render

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from django.db.models.signals import post_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 #from django_countries.fields import CountryField
@@ -23,7 +23,6 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-"""
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
    
@@ -32,4 +31,3 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     # Existing users: just save the profile
     instance.userprofile.save()
 
-"""
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
 
 
 
 
 
 
+
+@login_required
 def showOrders(request):
-    
+    profile = get_object_or_404(UserProfile, user=request.user)
+    #user_details = {
+        #'user': profile.user.username,  
+        #'user' : profile.user,
+        #'username': profile.user.username,
+        #'default_phone_number': profile.default_phone_number,    
+        #'default_postcode': profile.default_postcode,
+        #'default_town_or_city': profile.default_town_or_city, 
+        #'default_street_address1': profile.default_street_address1,
+        #'default_street_address2': profile.default_street_address2,
+        #'default_county': profile.default_county,   
+        # 
+        # #}
+    orders = profile.orders.all()
 
     template = 'account/profile.html'
-    context = {       
-                
-               
+    context = { 
+        #'user_details': user_details,    
+        'orders': orders,
+        'profile': profile,              
     }
+    print(f"The order details is {profile}")
    
 
     return render(request, template, context)
