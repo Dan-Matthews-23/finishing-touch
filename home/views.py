b5108391d56fbcb0971b47d477cab8976efd8939
Dan Matthews <dan.matthews1987@gmail.com>
2024-03-22 10:34:51 +0000

    UPDATED index.html, CREATED confirmation_email txt files, UPDATED webhook_handler to handle confirmation emails

--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from products.models import Products, Category
 
-def index(request):
-    return render(request, 'home/index.html')
+def home(request):
+    sandwich_items = Products.objects.all()
+    context = {
+        'sandwich_items' : sandwich_items,
+    }    
+    return render(request, 'home/index.html', context)
