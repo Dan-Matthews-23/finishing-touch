42ec9fbc925f81d75738e94b0f0993240282025e
Dan Matthews <dan.matthews1987@gmail.com>
2024-04-18 13:09:42 +0000

    UPDATED all checkout scripts in attempt to correct errors with Stripe processing payment

--- checkout/views.py
+++ checkout/views.py
@@ -160,19 +160,10 @@ def process_checkout(request):
         total = sum(float(item['price']) for item in basket)
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
-
-
-
-        
-
-
-        
-        
-        # Attempt to assign final Stripe variables
         intent = stripe.PaymentIntent.create(
-                        amount=stripe_total,
-                        currency=settings.STRIPE_CURRENCY,
-                    )
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
         template = 'checkout/order_confirmed.html'
         context = {
         #'order_form': order_form,
@@ -202,15 +193,7 @@ def process_checkout(request):
                 
         
         
-        
-               
-        
-        #if not form.is_valid():
-            #messages.error(request, ('There was an error with your form. '
-                                    
-        #else:            
-            #form.save()     
-            
+
            
                         
             
@@ -258,47 +241,22 @@ def process_checkout(request):
 
 
 
-def checkout_success(request):
+def checkout_success(request, order_number):
     """
     Handle successful checkouts
     """
-    request.session.get('save_info')
-    order_number = request.session['order_number']
-    form = get_object_or_404(Orders, order_number=order_number)
-
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
-        # Attach the user's profile to the form
-        form.user_profile = profile
-        form.save()
-
-        # Save the user's info
-        if save_info:
-            profile_data = {
-                'default_phone_number': form.phone_number,
-                'default_country': form.country,
-                'default_postcode': form.postcode,
-                'default_town_or_city': form.town_or_city,
-                'default_street_address1': form.street_address1,
-                'default_street_address2': form.street_address2,
-                'default_county': form.county,
-            }
-            user_profile_form = UserProfileForm(profile_data, instance=profile)
-            if user_profile_form.is_valid():
-                user_profile_form.save()
-
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
     messages.success(request, f'Order successfully processed! \
-        Your form number is {order_number}. A confirmation \
-        email will be sent to {form.email}.')
-    del request.session['order_items']
-    del request.session['basket']
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
-    #if 'basket' in request.session:
-        #del request.session['basket']
+    if 'cart' in request.session:
+        del request.session['basket']
 
     template = 'checkout/order_confirmed.html'
     context = {
-        'form': form,
+        'order': order,
     }
 
     return render(request, template, context)
@@ -310,15 +268,15 @@ def checkout_success(request):
 @require_POST
 def cache_checkout_data(request):
     try:
-        secret_id = request.POST.get('client_secret').split('_secret')[0]
+        pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
-        
-        stripe.PaymentIntent.modify(
-            secret_id, metadata={            
+        stripe.PaymentIntent.modify(pid, metadata={
+            'basket': json.dumps(request.session.get('basket', {})),
             'save_info': request.POST.get('save_info'),
             'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'There was an error while processing your payment. Please try again in a few minutes.')
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
