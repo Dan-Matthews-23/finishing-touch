a4e6aec603ca335cddb3cf0894e49c89dc574835
Dan Matthews <dan.matthews1987@gmail.com>
2024-04-24 10:21:01 +0000

    UPDATED accounts/order_history, /views with new formatting, CREATED accounts.css with styling

--- accounts/views.py
+++ accounts/views.py
@@ -37,23 +37,20 @@ def order_history(request, order_number):
     order = get_object_or_404(Orders, order_number=order_number)
     try:
         reviews = Reviews.objects.filter(order=order)
-        print("Reviews found:", reviews)  # Print the queryset
-        for review in reviews:            # Check individual review contents
-            print(review.review_title, review.review_body, review.stars) 
+        if reviews:
+            print("Reviews found:", reviews)  
+            for review in reviews:            
+                print(review.review_title, review.review_body, review.stars) 
+            star_range = range(review.stars) 
+        else: 
+            star_range = range(1)  # Create a range even if no stars
     except Reviews.DoesNotExist:
-        reviews = None  
-        #print("No reviews found.") 
-
-    # Get related order line items
-    order_line_items = order.lineitems.all() 
-
-    messages.info(request, (
-        f'This is a past confirmation for order number {order_number}. '
-        'A confirmation email was sent on the order date.'
-    ))
+        reviews = None 
+    order_line_items = order.lineitems.all()     
     template = 'account/order_history.html'
     context = {
         'order': order,
+        'star_range': star_range,
         'order_line_items': order_line_items,
         'reviews': reviews,
         'from_profile': True,
@@ -67,22 +64,17 @@ def order_history(request, order_number):
 
 @login_required
 def leave_review(request, order_number):
-    if request.method == 'POST':
-       
+    if request.method == 'POST':       
         order = get_object_or_404(Orders, order_number=order_number)  
-        if not request.POST.get('review_title') or not request.POST.get('review_body') or not request.POST.get('selected_rating', 0):
-                messages.error(request,
-                           ('Please fill out all fields'))
-                return redirect(request.META.get('HTTP_REFERER'))      
+        if not request.POST.get('review_title') or not request.POST.get('review_body') or not request.POST.get('selected_rating', 0):           
+            messages.error(request,('Please fill out all fields'))
+            return redirect(request.META.get('HTTP_REFERER'))      
         try:
-            selected_rating = int(request.POST.get('selected_rating', 0))  # Get the rating
-            #product_id = request.POST.get('product_id')
+            selected_rating = int(request.POST.get('selected_rating', 0))            
             review_title = request.POST.get('review_title')
-            review_body = request.POST.get('review_body')
-            #product = get_object_or_404(Products, pk=product_id)
+            review_body = request.POST.get('review_body')           
             add_review = Reviews(order=order, stars=selected_rating, review_title=review_title, review_body=review_body) 
-            add_review.save()  # Save the correct object 
-            print(f"Your review was created")
+            add_review.save()  # Save the correct object             
         except Exception as e:  
             print(f"There was an error while saving your review: {e}")
         return redirect(request.META.get('HTTP_REFERER'))
