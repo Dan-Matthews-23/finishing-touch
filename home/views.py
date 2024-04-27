4eae5a49aac3b8aa07e7971f61423ed42879369b
Dan Matthews <dan.matthews1987@gmail.com>
2024-04-27 11:57:42 +0000

    UPDATED /home with python linting

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from products.models import Products, Category
 
+
 def home(request):
     sandwich_items = Products.objects.all()
     context = {
-        'sandwich_items' : sandwich_items,
-    }    
+        'sandwich_items': sandwich_items,
+    }
     return render(request, 'home/index.html', context)
