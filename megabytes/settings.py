c2f2f2beb963c86bb9052153bbe422ef6bc987df
Dan Matthews <dan.matthews1987@gmail.com>
2024-04-27 17:06:49 +0000

    CREATED custom_storages, UPDATED settings to configure for AWS webserver for static files

--- megabytes/settings.py
+++ megabytes/settings.py
@@ -158,7 +158,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'megabytes-bfd0afc9e4a4.herokuapp'
-    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
