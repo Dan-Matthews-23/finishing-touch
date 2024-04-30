37ee7f36a68ec68a8bf0d517ab5bc5900653dc08
Dan Matthews <dan.matthews1987@gmail.com>
2024-04-30 16:41:03 +0000

    UPDATED settings to revert back to previous

--- megabytes/settings.py
+++ megabytes/settings.py
@@ -1,20 +1,21 @@
 import os
+import dj_database_url
 from pathlib import Path
 
-if os.path.exists("env.py"):
-    import env
+#if os.path.exists("env.py"):
+    #import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-z6=tya0-8l-&!w4wgc&pp_shtbwe1%xq-y=32d2w+xdf34%b@-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-danmatthews23-megabytes-hggui7x2owz.ws-eu110.gitpod.io']
-CSRF_TRUSTED_ORIGINS = ['https://8000-danmatthews23-megabytes-hggui7x2owz.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-danmatthews23-megabytes-hggui7x2owz.ws-eu110.gitpod.io', 'megabytes-bfd0afc9e4a4.herokuapp.com', 'https://megabytes-bfd0afc9e4a4.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://8000-danmatthews23-megabytes-hggui7x2owz.ws-eu110.gitpod.io', 'megabytes-bfd0afc9e4a4.herokuapp.com', 'https://megabytes-bfd0afc9e4a4.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
     'accounts',
     'crispy_forms',
     'crispy_bootstrap5',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -87,7 +89,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
@@ -95,12 +97,19 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'megabytes.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -136,14 +145,35 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'products/static'),
-    os.path.join(BASE_DIR, 'static'),
-]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'megabytes-dan'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
+
+
 
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
